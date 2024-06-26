11170c63e12b1db93bebe0e29c540ba665949840
Soranosuke2001 <sorazora1.4@gmail.com>
2024-06-26 15:12:53 +0900

    test app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def home():
+    return "this is LINE API BOT"
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8000)
