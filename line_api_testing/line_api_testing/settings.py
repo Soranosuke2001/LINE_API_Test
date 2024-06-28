ed4a810da968c24233b116fd1bb983f507740b75
Soranosuke2001 <sorazora1.4@gmail.com>
2024-06-28 14:20:35 +0900

    moved to env file

--- line_api_testing/line_api_testing/settings.py
+++ line_api_testing/line_api_testing/settings.py
@@ -10,12 +10,15 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
+
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-print("Running settings")
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -26,12 +29,7 @@ SECRET_KEY = "django-insecure-&*lj4t-ao44lq&x&7*3_7=ro2qwt-c#%q7u-35e1ndn2*y%$hl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-  "gitlabce-acit4850-group21-uswest1-google-cloud.com",
-  "3.113.13.92",
-  "localhost",
-  "127.0.0.1",
-]
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(',')
 
 
 # Application definition
