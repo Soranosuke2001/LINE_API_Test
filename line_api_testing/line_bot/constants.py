10b8064add442a52efe2822d09611ad40a9a350c
Soranosuke2001 <sorazora1.4@gmail.com>
2024-06-29 11:56:49 +0900

    debugging

--- line_api_testing/line_bot/constants.py
+++ line_api_testing/line_bot/constants.py
@@ -1,5 +1,5 @@
 ROUTES = {
-  "image": "image_handler",
+  "image": "line_bot:image_handler",
   "video": "line_bot/submit/video/",
   "file": "line_bot/submit/file/",
   "audio": "line_bot/submit/audio/",
