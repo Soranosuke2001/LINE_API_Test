832cbec9b2b0519386129faade9ae4c65d4e8af9
Soranosuke2001 <sorazora1.4@gmail.com>
2024-06-29 18:37:13 +0900

    fixed view error

--- line_api_testing/s3_handler/urls.py
+++ line_api_testing/s3_handler/urls.py
@@ -5,8 +5,8 @@ from . import views
 app_name = 's3_handler'
 
 urlpatterns = [
-  path("upload/image/", views, name='image_upload'),
-  path("upload/video/", views, name='video_upload'),
-  path("upload/file/", views, name='file_upload'),
-  path("upload/audio/", views, name='audio_upload'),
+  path("upload/image/", views.S3ImageUploadEvent.as_view(), name='image_upload'),
+  # path("upload/video/", views, name='video_upload'),
+  # path("upload/file/", views, name='file_upload'),
+  # path("upload/audio/", views, name='audio_upload'),
 ]
