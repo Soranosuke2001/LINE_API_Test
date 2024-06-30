aadc1d35f1f21bea80c863a80ec7ef36c152765b
Soranosuke2001 <sorazora1.4@gmail.com>
2024-06-30 13:02:39 +0900

    connected url to view

--- line_api_testing/s3_handler/urls.py
+++ line_api_testing/s3_handler/urls.py
@@ -6,7 +6,7 @@ app_name = 's3_handler'
 
 urlpatterns = [
   path("upload/image/", views.S3ImageUploadEvent.as_view(), name='image_upload'),
-  # path("upload/video/", views, name='video_upload'),
+  path("upload/video/", views.S3VideoUploadEvent.as_view(), name='video_upload'),
   # path("upload/file/", views, name='file_upload'),
   # path("upload/audio/", views, name='audio_upload'),
 ]
