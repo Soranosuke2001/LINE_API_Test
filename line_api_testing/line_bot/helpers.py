61f11286da49391973693365c4821831e16375d1
Soranosuke2001 <sorazora1.4@gmail.com>
2024-06-30 11:43:36 +0900

    functions to construct data obj for video, audio and file types

--- line_api_testing/line_bot/helpers.py
+++ line_api_testing/line_bot/helpers.py
@@ -10,6 +10,13 @@ from .constants import (
 )
 
 
+# Forward POST request
+def forward_request(url, data):
+  client = Client()
+  response = client.post(url, data, content_type='application/json')
+  return response
+
+
 # Verify the Signature
 def verify_signature(request, handler):
   if not request:
@@ -67,9 +74,76 @@ def construct_image_data(data):
   }
 
 
-# Forward POST request
-def forward_request(url, data):
-  client = Client()
-  response = client.post(url, data, content_type='application/json')
-  return response
+# Create filtered data object (video endpoint)
+def construct_video_data(data):
+  content_provider = data['message']['contentProvider']['type']
+  dt_obj = convert_timestamp(data['timestamp'])
+
+  if content_provider == 'line':
+    video_url = data['message']['id']
+    preview_image_url = ''
+  else:
+    video_url = data['message']['contentProvider']['originalContentUrl']
+    preview_image_url = data['message']['contentProvider']['previewImageUrl']
+
+  return {
+    "video_url": video_url,
+    "preview_image_url": preview_image_url,
+    "duration_ms": data['message']['duration'],
+    "content_provider": content_provider,
+    "source_type": data['source']['type'],
+    "reply_token": data['replyToken'],
+    "is_redelivery": data['deliveryContext']['isRedelivery'],
+    "user_id": data['source']['userId'],
+    "webhook_event_id": data['webhookEventId'],
+    "timestamp": dt_obj,
+  }
+
+
+# Create filtered data object (audio endpoint)
+def construct_audio_data(data):
+  content_provider = data['message']['contentProvider']['type']
+  dt_obj = convert_timestamp(data['timestamp'])
+
+  if content_provider == 'line':
+    audio_url = data['message']['id']
+  else:
+    audio_url = data['message']['contentProvider']['originalContentUrl']
+
+  return {
+    "audio_url": audio_url,
+    "duration_ms": data['message']['duration'],
+    "content_provider": content_provider,
+    "source_type": data['source']['type'],
+    "reply_token": data['replyToken'],
+    "is_redelivery": data['deliveryContext']['isRedelivery'],
+    "user_id": data['source']['userId'],
+    "webhook_event_id": data['webhookEventId'],
+    "timestamp": dt_obj,
+  }
+
+
+# Create filtered data object (file endpoint)
+def construct_file_data(data):
+  content_provider = data['message']['contentProvider']['type']
+  dt_obj = convert_timestamp(data['timestamp'])
+
+  if content_provider == 'line':
+    file_url = data['message']['id']
+  else:
+    file_url = data['message']['contentProvider']['originalContentUrl']
+
+  return {
+    "file_url": file_url,
+    "filename": data['message']['filename'],
+    "filesize_bytes": data['message']['filesize'],
+    "content_provider": content_provider,
+    "source_type": data['source']['type'],
+    "reply_token": data['replyToken'],
+    "is_redelivery": data['deliveryContext']['isRedelivery'],
+    "user_id": data['source']['userId'],
+    "webhook_event_id": data['webhookEventId'],
+    "timestamp": dt_obj,
+  }
+
 
