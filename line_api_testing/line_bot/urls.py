0c7fa438c1dd525ff75a93bf48aed999c806dd60
Soranosuke2001 <sorazora1.4@gmail.com>
2024-06-30 11:44:19 +0900

    urls for each event type

--- line_api_testing/line_bot/urls.py
+++ line_api_testing/line_bot/urls.py
@@ -7,7 +7,7 @@ app_name = "line_bot"
 urlpatterns = [
   path("", views.WebhookEvent.as_view(), name="webhook_handler"),
   path("submit/image/", views.LineImageEvent.as_view(), name="image_handler"),
-  path("submit/video/", views.LineImageEvent.as_view(), name="video_handler"),
-  path("submit/file/", views.LineImageEvent.as_view(), name="file_handler"),
-  path("submit/audio/", views.LineImageEvent.as_view(), name="audio_handler"),
+  path("submit/video/", views.LineVideoEvent.as_view(), name="video_handler"),
+  path("submit/audio/", views.LineAudioEvent.as_view(), name="audio_handler"),
+  path("submit/file/", views.LineFileEvent.as_view(), name="file_handler"),
 ]
