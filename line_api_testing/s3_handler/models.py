9c95eb114f3abf604c36676ee88007a018702765
Soranosuke2001 <sorazora1.4@gmail.com>
2024-06-30 12:19:26 +0900

    created models and serializers for each event type

--- line_api_testing/s3_handler/models.py
+++ line_api_testing/s3_handler/models.py
@@ -11,3 +11,33 @@ class S3LineImage(models.Model):
   def __str__(self):
     return self.image_id
 
+
+class S3LineVideo(models.Model):
+  video_id = models.CharField(max_length=256)
+  video_url = models.CharField(max_length=256)
+  user_id = models.CharField(max_length=256)
+  timestamp = models.DateTimeField()
+
+  def __str__(self):
+    return self.video_id
+
+
+class S3LineAudio(models.Model):
+  audio_id = models.CharField(max_length=256)
+  audio_url = models.CharField(max_length=256)
+  user_id = models.CharField(max_length=256)
+  timestamp = models.DateTimeField()
+
+  def __str__(self):
+    return self.audio_id
+
+
+class S3LineFile(models.Model):
+  file_id = models.CharField(max_length=256)
+  file_url = models.CharField(max_length=256)
+  user_id = models.CharField(max_length=256)
+  timestamp = models.DateTimeField()
+
+  def __str__(self):
+    return self.file_id
+
--- line_api_testing/s3_handler/serializers.py
+++ line_api_testing/s3_handler/serializers.py
@@ -1,7 +1,10 @@
 from rest_framework import serializers
 
 from .models import (
-  S3LineImage
+  S3LineImage,
+  S3LineVideo,
+  S3LineAudio,
+  S3LineFile
 )
 
 
@@ -11,6 +14,39 @@ class S3LineImageSerializer(serializers.ModelSerializer):
     fields = [
       "image_id",
       "image_url",
+      "user_id",
+      "timestamp",
+    ]
+
+
+class S3LineVideoSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = S3LineVideo
+    fields = [
+      "video_id",
+      "video_url",
+      "user_id",
+      "timestamp",
+    ]
+
+
+class S3LineAudioSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = S3LineAudio
+    fields = [
+      "audio_id",
+      "audio_url",
+      "user_id",
+      "timestamp",
+    ]
+
+
+class S3LineFileSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = S3LineFile
+    fields = [
+      "file_id",
+      "file_url",
       "user_id",
       "timestamp",
     ]
