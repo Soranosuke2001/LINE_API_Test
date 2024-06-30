39fe56173f1590aea353b942ca3589307ea6ea1b
Soranosuke2001 <sorazora1.4@gmail.com>
2024-06-30 13:42:22 +0900

    connected view to endpoint

--- line_api_testing/s3_handler/urls.py
+++ line_api_testing/s3_handler/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path
 
-from . import views
+from .views import (
+  S3ImageUploadEvent,
+  S3VideoUploadEvent,
+  S3AudioUploadEvent,
+  S3FileUploadEvent,
+)
 
 app_name = 's3_handler'
 
 urlpatterns = [
-  path("upload/image/", views.S3ImageUploadEvent.as_view(), name='image_upload'),
-  path("upload/video/", views.S3VideoUploadEvent.as_view(), name='video_upload'),
-  # path("upload/file/", views, name='file_upload'),
-  # path("upload/audio/", views, name='audio_upload'),
+  path("upload/image/", S3ImageUploadEvent.as_view(), name='image_upload'),
+  path("upload/video/", S3VideoUploadEvent.as_view(), name='video_upload'),
+  path("upload/audio/", S3AudioUploadEvent.as_view(), name='audio_upload'),
+  path("upload/file/", S3FileUploadEvent.as_view(), name='file_upload'),
 ]
