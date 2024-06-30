a799861a57cd30679b010ab81559c35a55e423d1
Soranosuke2001 <sorazora1.4@gmail.com>
2024-06-30 11:46:17 +0900

    models and corresponding serializer for each event type

--- line_api_testing/line_bot/serializers.py
+++ line_api_testing/line_bot/serializers.py
@@ -1,7 +1,10 @@
 from rest_framework import serializers
 
 from .models import (
-  LineImage
+  LineImage,
+  LineVideo,
+  LineAudio,
+  LineFile
 )
 
 
@@ -19,4 +22,53 @@ class LineImageSerializer(serializers.ModelSerializer):
       "timestamp",
     ]    
 
-    
+
+class LineVideoSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = LineVideo
+    fields = [
+      "video_url",
+      "preview_image_url",
+      "duration_ms",
+      "content_provider",
+      "source_type",
+      "reply_token",
+      "is_redelivery",
+      "user_id",
+      "webhook_event_id",
+      "timestamp",
+    ]    
+
+
+class LineAudioSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = LineAudio
+    fields = [
+      "audio_url",
+      "duration_ms",
+      "content_provider",
+      "source_type",
+      "reply_token",
+      "is_redelivery",
+      "user_id",
+      "webhook_event_id",
+      "timestamp",
+    ]    
+
+
+class LineFileSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = LineFile
+    fields = [
+      "file_url",
+      "filename",
+      "filesize_bytes",
+      "content_provider",
+      "source_type",
+      "reply_token",
+      "is_redelivery",
+      "user_id",
+      "webhook_event_id",
+      "timestamp",
+    ]    
+
