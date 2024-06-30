74495a96288b19fadeffdc91d220399f784da791
Soranosuke2001 <sorazora1.4@gmail.com>
2024-06-30 13:12:32 +0900

    debugging

--- line_api_testing/s3_handler/helpers.py
+++ line_api_testing/s3_handler/helpers.py
@@ -72,10 +72,7 @@ def fetch_binary_data(id):
 
 # Created filtered data object
 def construct_filtered_data(data):
-    # id = data[data['message']['type']]
-    print(data)
-    print()
-    id = '1'
+    id = data['message']['id']
     user_id = data['user_id']
     timestamp, month_taken = get_month(data['timestamp'])
     object_path = f'{user_id}/{month_taken}/{id}'
